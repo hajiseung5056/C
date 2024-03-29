c56af4c6818518b39dc7bfc7ea2e26bea342589f
하지승 <cjswp051@naver.com>
2024-03-29 16:18:42 +0900

    5일차

---
+++ 23_1_1.java
@@ -0,0 +1,22 @@
+class Static{
+ 
+    public int a = 20;
+    static int b = 0;
+    
+}
+ 
+class Main{
+    public static void main(String[] args) {
+        
+        int a;
+        a = 10;
+        Static.b = a;
+ 
+        Static st = new Static();
+ 
+        System.out.println(Static.b++);
+        System.out.println(st.b);
+        System.out.println(a);
+        System.out.print(st.a);
+    }
+}
